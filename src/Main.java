8c55326c767e49a8d7c428bbd24a6e534512f960
choihana <chhana0703@gmail.com>
2023-04-05 21:05:40 +0900

    while문 삽입했습니다

--- src/Main.java
+++ src/Main.java
@@ -24,3 +24,38 @@ public class Main {
         }
     }
 }
+
+    int chance = 1 ;
+
+        while(true){
+
+                int strike = 0;
+                int ball = 0;
+//            int out = 0;
+
+                System.out.print(chance+ "번째 시도 : ");
+
+                String answer = scanner.nextLine();
+
+                for(int i=0;i<3;i++){
+        if(gameNumber.charAt(i)==answer.charAt(i)){
+        strike+=1;
+        }else{
+        if(answer.indexOf(gameNumber.charAt(i))!=-1){
+        ball+=1;
+        }
+        }
+        }
+
+        if(strike==3){
+        System.out.println(chance+ "번만에 맞히셨습니다.");
+        System.out.println("게임을 종료합니다.");
+        break;
+        }else{
+        System.out.println(+ball+"B"+strike+"S");
+        }
+
+        chance+=1;
+        }
+        }
+        }
